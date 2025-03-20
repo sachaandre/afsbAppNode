1b27db223d23a0fb7986f416f01e1f8c4473cc09
sachaandre <andresachaa@gmail.com>
2025-03-20 22:39:02 +0100

    express app creation

---
+++ afsbApp/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'AFSB Contrib to Osuny' });
+});
+
+module.exports = router;
